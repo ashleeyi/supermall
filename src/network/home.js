1552028d9fda065c5fed966205247cd3b2f5802a
yi_juan <yi_juan@hisuntech.com>
2020-03-15 21:06:17 +0800

    发多少

---
+++ src/network/home.js
@@ -0,0 +1,18 @@
+import { request } from './request';
+export function getHomeMultiData() {
+  return request({
+    url: '/home/multidata',
+    method: 'get'
+  })
+};
+
+export function getHomeGoods(type, page) {
+  return request({
+    url: '/home/data',
+    method: 'get',
+    params: {
+      type,
+      page
+    }
+  })
+}
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item"  v-for="(item,index) in recommends" :key="index">
+      <a class="recommend-link" :href="item.link">
+        <img class="recommend-img" :src="item.image"/>
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name :'RecommendView',
+    props :{
+      recommends:{
+        type : Array,
+        default(){
+          return []
+        }
+      }
+    }
+}
+</script>
+
+<style scoped>
+.recommend{
+  display: flex;
+  padding:15px 0px 30px;
+  border-bottom: 10px solid #ffffff;
+  height:70px;
+
+}
+.recommend-item{
+  flex:1;
+  text-align: center;
+  font-size: 12px;;
+}
+.recommend-img{
+  width:70px;
+  height:70px;
+  margin-bottom: 10px;;
+}
+</style>
